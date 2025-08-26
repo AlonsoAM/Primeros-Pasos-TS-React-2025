7e058d8a3ad3a28aa0edfc65edf22af94c3943b4
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-08-25 21:54:38 -0500

    🛠️ feat: agregar estado y lógica en `ItemCounter`
    |- Añadido estado para controlar la cantidad de ítems (`useState`).
    |- Implementadas funciones `handleAdd` y `handleRemove` para ajustar la cantidad.
    |- Actualizado JSX para usar las nuevas funciones y reflejar cambios dinámicos.

--- src/shopping-car/ItemCounter.tsx
+++ src/shopping-car/ItemCounter.tsx
@@ -1,9 +1,23 @@
+import {useState} from "react";
+
 interface ItemCounterProps {
   name: string
   quantity?: number
 }
 
 export const ItemCounter = ({name, quantity}: ItemCounterProps) => {
+
+  const [count, setCount] = useState(quantity ?? 1)
+
+  const handleAdd = () => {
+    setCount(count + 1)
+  }
+
+  const handleRemove = () => {
+    if (count === 1) return
+    setCount(count - 1)
+  }
+
   return (
     <section style={{
       display: 'flex',
@@ -12,9 +26,9 @@ export const ItemCounter = ({name, quantity}: ItemCounterProps) => {
       marginTop: '10px',
     }}>
       <span style={{width: 150}}>{name}</span>
-      <button>+1</button>
-      <span>{quantity}</span>
-      <button>-1</button>
+      <button onClick={handleAdd}>+1</button>
+      <span>{count}</span>
+      <button onClick={handleRemove}>-1</button>
     </section>
   )
 }
